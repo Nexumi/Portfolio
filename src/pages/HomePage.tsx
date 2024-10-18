ea704a21599303b85845b5f17944a7450252248d
Jimmy Pan <jimmypan13570@gmail.com>
2024-10-18 14:14:45 -0700

    Update Home Page w/ Footer

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Intro from "../components/Intro";
 
 export default function HomePage() {
   return (
-    <>
+    <div class="h-screen flex flex-col">
       <Header />
       <Intro />
-    </>
+      <Footer />
+    </div>
   );
 }
